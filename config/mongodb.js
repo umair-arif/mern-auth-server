0a41ddf81f43e625e936816ced623bdf5c7d5413
umair-arif <umair.aarifs@gmail.com>
2025-07-17 21:38:12 +0500

    add hard code mongo url

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  const MONGODB_URL =
+    "mongodb+srv://umairarif:DupgbVWx1CE5FfiU@umaircluster0.wjzfetl.mongodb.net/mern-auth?retryWrites=true&w=majority&appName=UmairCluster0";
   mongoose.connection.on("connected", () => console.log("Database connected"));
-  await mongoose.connect(process.env.MONGODB_URL);
+  await mongoose.connect(MONGODB_URL);
 };
 
 export default connectDB;
