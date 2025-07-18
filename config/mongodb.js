73b144fd334f68b4bf0c675095fe06428297fcbc
umair-arif <umair.aarifs@gmail.com>
2025-07-18 11:48:58 +0500

    add env variable for mongourl

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const MONGODB_URL =
-    "mongodb+srv://umairarif:DupgbVWx1CE5FfiU@umaircluster0.wjzfetl.mongodb.net/mern-auth?retryWrites=true&w=majority&appName=UmairCluster0";
   mongoose.connection.on("connected", () => console.log("Database connected"));
-  await mongoose.connect(MONGODB_URL);
+  await mongoose.connect(process.env.MONGODB_URL);
 };
 
 export default connectDB;
