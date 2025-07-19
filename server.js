6be6df59a0316ba28414adea196120cb548517f7
umair-arif <umair.aarifs@gmail.com>
2025-07-19 14:09:58 +0500

    add url in server file

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowOrigins = [process.env.FRONTEND_URL];
+const allowOrigins = ["https://mern-auth-client-iota.vercel.app"];
 
 app.use(express.json());
 app.use(cookieParser());
