f85f41ba2118ff767bc8ab6b752483de176445b1
umair-arif <umair.aarifs@gmail.com>
2025-07-19 11:26:28 +0500

    update vercel.json and api/index.js file

--- api/index.js
+++ api/index.js
@@ -1,28 +1,26 @@
+import "dotenv/config";
 import express from "express";
-import serverless from "serverless-http";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import "dotenv/config";
-import connectDB from "../config/mongodb.js";
-import authRouter from "../routes/authRoutes.js";
-import userRouter from "../routes/userRoutes.js";
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
-// Setup Express
 const app = express();
+const port = process.env.PORT || 4000;
 connectDB();
+
+const allowOrigins = ["http://localhost:5173"];
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL, // Make sure this is defined in Vercel env
-    credentials: true,
-  })
-);
+app.use(cors({ origin: allowOrigins, credentials: true }));
 
-// Routes
-app.use("/auth", authRouter);
-app.use("/user", userRouter);
-app.get("/", (req, res) => res.send("API Running on Vercel"));
-
-export default serverless(app);
+// api end point
+app.get("/", (req, res) => {
+  res.send("api working");
+});
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.listen(port, () => console.log(`Server started on port:${port}`));
+module.exports = app;
